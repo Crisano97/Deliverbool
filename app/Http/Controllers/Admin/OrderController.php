52f6dc504694031d1906ef3d5d52a4ff776ea648
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-10-14 17:34:59 +0200

    create orderIndex.blade

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
@@ -14,7 +17,9 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $restaurants = Restaurant::where('user_id', Auth::id())->get();
+        $orders = Order::where('restaurant_id', Auth::id())->get();
+        return view('admin.restaurants.orderIndex', compact(['orders', 'restaurants']));
     }
 
     /**
