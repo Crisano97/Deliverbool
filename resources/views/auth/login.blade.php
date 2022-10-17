1e030eae4a5b22a54733e53dba492588b5da211d
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-10-17 18:02:15 +0200

    center btn login&register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,7 +60,7 @@
                                 </div>
 
                                 <div class="form-group row mb-0">
-                                    <div class="col-md-8 offset-md-4">
+                                    <div class="col-md-8 offset-md-4 d-flex justify-content-center">
                                         <button type="submit" class="btn btn-primary">
                                             {{ __('Login') }}
                                         </button>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -81,7 +81,7 @@
                                 </div>
 
                                 <div class="form-group row mb-0">
-                                    <div class="col-md-6 offset-md-4">
+                                    <div class="col-md-6 offset-md-4 d-flex justify-content-center">
                                         <button type="submit" class="btn btn-primary">
                                             {{ __('Register') }}
                                         </button>
