52778a8b733081c488e0275f4c003df2b34f6c2b
Franpan94 <francescopanico94@libero.it>
2022-10-18 16:49:34 +0200

    create subComponets-main

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,17 +1,56 @@
 <template>
-  <header>Ciao header
-
-    <router-link class="nav-link" to="/">Home</router-link>
-
+  <header>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand logo-brand" href="#">
+          <img
+            class="img-brand border border-warning border-3 rounded-circle"
+            src="../../../public/assets/images/DElivebool.png"
+            alt=""
+          />
+          DeliveBool
+        </a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <div class="d-flex justify-content-end w-100">
+            <form class="d-flex" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <div>
+                <i class="fa-solid fa-user p-2 icon"></i>
+              </div>
+              <div>
+                <i class="fa-solid fa-store p-2 icon"></i>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
   </header>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+header {
+  background-color: #ffbd59;
+}
+.logo-brand {
+  font-family: "Bebas Neue", sans-serif;
+  font-size: x-large;
+}
+.img-brand {
+  width: 50px;
+  object-fit: cover;
+}
+.icon {
+  font-size: 20px;
+  color: black;
+}
 </style>
--- resources/js/components/MainComponent.vue
+++ resources/js/components/MainComponent.vue
@@ -1,13 +1,26 @@
 <template>
   <main>
-     Ciao Main
+    <HeroComponent />
+    <CategoryComponent />
+    <DownloadAppComponent />
+    <RestaurantComponent />
   </main>
 </template>
 
 <script>
-export default {
+import HeroComponent from './SubComponentsMain/HeroComponent.vue';
+import CategoryComponent from './SubComponentsMain/CategoryComponent.vue';
+import DownloadAppComponent from './SubComponentsMain/DownloadAppComponent.vue';
+import RestaurantComponent from './SubComponentsMain/RestaurantComponent.vue';
 
-}
+export default {
+  components: {
+    HeroComponent,
+    CategoryComponent,
+    DownloadAppComponent,
+    RestaurantComponent
+  }
+};
 </script>
 
 <style>
