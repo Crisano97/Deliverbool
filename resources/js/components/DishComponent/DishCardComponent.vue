e077fdb715f91d973adcbb7fe0caca71c92cf78e
Bice93 <bianca.cerulo@hotmail.it>
2022-10-20 18:02:37 +0200

    Style dish card

--- resources/js/components/DishComponent/DishCardComponent.vue
+++ resources/js/components/DishComponent/DishCardComponent.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="p-2">
-    <div class="card p-2">
-      <img :src="dish.image" class="card-img-top" alt="..." />
-      <div class="card-body text-center">
-        <h5>{{ dish.name }}</h5>
+    <div class="card p-3 h-100">
+      <div class="box_iamge">
+       <img :src="dish.image" class="card-img-top" alt="..."  />
       </div>
-      <button class="btn btn-primary button-plus-dish">
-        <i class="fas fa-plus"></i>
-      </button>
+      <div class="card-body p-0 mt-2">
+        <h5 class="fw-bold text-center">{{ dish.name }}</h5>
+        <p class="text-start"><span class="fw-normal fw-semibold">Ingredienti:</span> {{ dish.ingredients }} </p>
+      </div>
+        <div class="d-flex justify-content-between">
+          <p class="m-0 align-self-center ">€ {{ dish.price.toFixed(2) }}</p>
+          <button class="btn btn-primary button-plus">
+            <i class="fas fa-plus"></i>
+          </button>
+        </div>
     </div>
   </div>
 </template>
@@ -31,7 +37,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.button-plus-dish {
+.button-plus {
   color: black;
   background-color: rgb(56, 193, 114, 0.2);
   color: #38c172;
@@ -41,5 +47,13 @@ export default {
     background-color: #38c172;
     color: white;
   }
+}
+
+img{
+  object-fit: contain;
+  transition: 1s;
+}
+.card:hover img{
+  transform: scale(1.08);
 }
 </style>
