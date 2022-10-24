a514bccfe3073bb5e085a57c17162c62ac5ece04
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-10-24 21:05:08 +0200

    change style on payment card

--- resources/js/pages/CheckoutPage.vue
+++ resources/js/pages/CheckoutPage.vue
@@ -34,7 +34,7 @@
                         <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Telefono *</label>
                             <input pattern="[0-9]{10}" type="tel" class="form-control" v-model="telefono" id="exampleFormControlInput1" placeholder="Inserisci il tuo numero di telefono" required>
-                            <small id="emailHelp" class="form-text text-muted">ES. 333 333 33 33</small>
+                            <small id="emailHelp" class="form-text text-muted">ES.3923346509</small>
                         </div>  
                         <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Indirizzo di consegna *</label>
@@ -48,35 +48,13 @@
                     <div>
 
                     </div>
-                    <!-- <div class="m-4 bg-check text-center p-3 rounded">
-                        <div class="border-bottom">
-                            <h6>Totale</h6>
-                            <h4>{{ totlaPrice.toFixed(2)}}</h4>
-                        </div>
-                        <div class="pt-2">
-                            <div v-for="dish in dishes" :key="dish.id">
-                                <span>{{dish.quantity}}°</span>
-                                {{ dish.name }}
-                            </div>
-                        </div>
-                        <div class="pt-2">
-                            <button class="btn btn-outline-success" @click="sendOrder()" :disabled="isSent">
-                                invia l'ordine
-                            </button>
-                        </div>
- 
-                    </div> -->
                     <div class="card mt-3 card-braintree">
                             
                             
                             <div id="dropin-container"></div>
-                            <button id="submit-button" type="submit" class="button button--small button--green">Acquista</button>
-                            <!-- <input
-                                class="input-pay"
-                                type="submit"
-                                @click="beforeBuy()"
-                                value="Paga"
-                            /> -->
+                            <div class="d-flex justify-content-center">
+                                <button id="submit-button" type="submit" class="btn btn-outline-success col-3">Acquista</button>
+                            </div>
                         </div>
                 </div>
             </div>
@@ -149,8 +127,6 @@ export default {
                 this.arr.forEach((element)=>{
                     order.push(JSON.stringify(element));
                 })
-                // let amount = this.totlaPrice.toFixed(2);
-                // console.log('ciao')
                 axios.post("/api/order/save",{
                     customer_name: this.name,
                     customer_email: this.email,
@@ -161,46 +137,15 @@ export default {
                     restaurant_id: this.restaurant_id,
                     
                 }).then((results)=>console.log(results)).catch(error => {
-                    // console.log('dio')
+                    window.alert('ERRORE: ALCUNI DEI CAMPI INSERITI NON SONO VALIDI, RIPROVA')
                     console.log("ERRRR:: ",error.response.data);
                 });
                 }
             }).catch(error => {
                     console.log("ERRRR:: ",error.response.data);
+                    window.alert('ERRORE: ALCUNI DEI CAMPI INSERITI NON SONO VALIDI, RIPROVA')
                 });
         },
-        // sendOrderEmail(){
-        //     console.warn('invio form');
-        //     this.isSent = true;
-
-        //     axios.post('api/order/save', {
-        //         // 'name' : this.name,
-        //         // 'email' : this.email,
-        //         // 'telefono' : this.telefono,
-        //         // 'indirizzo' : this.indirizzo,
-        //     }).then((response)=>{
-        //         console.log(response);
-        //     }).catch((error)=>{
-        //         console.warn(error);
-        //     })
-        // },
-        // onSuccess(payload) {
-        //     // payload.nonce = this.cart.reduce((total, dish) => total + dish.price * dish.quantity, 0)
-        //     // console.log(payload)
-        //     let nonce = payload.nonce;
-        //     // console.log(nonce)
-        //     let amount = this.totlaPrice
-        //         .toFixed(2);
-        //     //axios call per riempire database e poi parte pagamento?
-        //     axios.post(
-        //         "http://127.0.0.1:8000/api/order/make/payment?" + this.tokenApi+ "&amount=" +
-        //             amount
-        //     ).then((result) => {
-        //         this.allDone = true;
-        //         // console.log(result)
-        //         this.sendForm();
-        //     });
-        // },
         onError(error) {
             let message = error.message;
             // Whoops, an error has occured while trying to get the nonce
@@ -210,29 +155,6 @@ export default {
             let my_btn = document.getElementById("v-btn");
             my_btn.click();
         },
-        // sendOrder(){
-        //     if(this.allDone){
-        //         let order = [];
-        //         this.arr.forEach((element)=>{
-        //             order.push(JSON.stringify(element));
-        //         })
-        //         // let amount = this.totlaPrice.toFixed(2);
-                
-        //         axios.post("/api/order/save",{
-        //             customer_name: this.name,
-        //             customer_email: this.email,
-        //             customer_phone_number: this.telefono,
-        //             customer_address: this.indirizzo,
-        //             order: order,
-        //             total_price: this.totlaPrice, 
-        //             restaurant_id: this.restaurant_id,
-        //         }).then((results)=>console.log(results)).catch(error => {
-        //             console.log("ERRRR:: ",error.response.data);
-        //         });
-        //         // console.log(order)
-        //     }
-        // },
-
           getTotal(){
         this.totlaPrice = 0,
         this.dishes.forEach(element => {
@@ -249,18 +171,8 @@ export default {
 
 <style scoped>
 .card-braintree {
-    border: 1px solid #ffbd59;
-    background-color: rgb(56, 193, 114, 0.2);
-    border-radius: 20px 20px 50px 50px !important;
-    padding: 0 2rem;
-}
-.input-pay {
-    margin: 1rem 20rem;
-    color: white;
     background-color: #ffbd59;
-    box-shadow: 2px 2px #88888865;
-    border-radius: 25px;
-    padding: 0.5rem 3rem;
+    padding: 5px;
 }
 .bg-check{
     background-color: #ffbd59;
